9d51449e6ca5a25e880d9860d16b3048c080cd00
kgdpete2022 <mail4dev2022@gmail.com>
2023-05-23 17:01:27 +0200

    Errors - 6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,9 +3,8 @@ from django.db.models import F
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
+from rest_framework import serializers
 
 User = get_user_model()
 
