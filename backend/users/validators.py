9b34c30d9dda44293ee59546508b4d4d42f72619
kgdpete2022 <mail4dev2022@gmail.com>
2023-05-26 15:17:31 +0200

    Remaking backend from scratch

--- backend/users/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_username(value):
-    if value == 'me':
-        raise ValidationError(
-            'Имя пользователя "me" зарезервировано.'
-        )
-    return value
